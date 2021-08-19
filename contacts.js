60e11821a82531453decb5d931c30bfbe2b1bd08
Volodymyr Viter <mad.tvido@gmail.com>
2021-08-19 20:02:40 +0300

    upd

--- contacts.js
+++ contacts.js
@@ -1,5 +1,8 @@
 const fs = require("fs").promises;
 const path = require("path");
+const { v4 } = require("uuid");
+
+const { contactsList, updateContacts } = require("./module/module");
 
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
@@ -9,21 +12,71 @@ const contactsPath = path.join(__dirname, "./db/contacts.json");
  */
 
 // TODO: задокументировать каждую функцию
-function listContacts() {
-  // ...твой код
-}
+const listContacts = async () => {
+  try {
+    const contacts = await contactsList(contactsPath);
+    console.table(contacts);
+    return contacts;
+  } catch (error) {
+    throw error;
+  }
+};
 
-function getContactById(contactId) {
-  // ...твой код
-}
+const getContactById = async (contactId) => {
+  try {
+    const contsctsList = await contactsList(contactsPath);
+    const selectContact = contsctsList.find(
+      (item) => String(item.id) === String(contactId)
+    );
+    if (!selectContact) {
+      throw new Error("There is no product with this id");
+    }
+    console.table(selectContact);
+    return selectContact;
+  } catch (error) {
+    throw error;
+  }
+};
 
-function removeContact(contactId) {
-  // ...твой код
-}
+const removeContact = async (contactId) => {
+  try {
+    const contsctsList = await contactsList(contactsPath);
+    const idx = contsctsList.findIndex(
+      (item) => String(item.id) === String(contactId)
+    );
+    if (!idx === -1) {
+      throw new Error("There is no product with this id");
+    }
+    const newContacts = contsctsList.filter(
+      (item) => String(item.id) !== String(contactId)
+    );
+    await updateContacts(newContacts, contactsPath);
+    console.table(newContacts);
+    return contsctsList[idx];
+  } catch (error) {
+    throw error;
+  }
+};
 
-function addContact(name, email, phone) {
-  // ...твой код
-}
+const addContact = async (name, email, phone) => {
+  try {
+    const contsctsList = await contactsList(contactsPath);
+    const newContact = {
+      id: v4(),
+      name: name,
+      email: email,
+      phone: phone,
+    };
+
+    const newContactsList = [...contsctsList, newContact];
+    await updateContacts(newContactsList, contactsPath);
+
+    console.table(newContact);
+    return newContact;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
   listContacts,
